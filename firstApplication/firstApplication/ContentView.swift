f6b64c0add3d222d533d7ae43df740d3405b3ee0
bajpaiNikhil <nikhil.cse16@gmail.com>
2025-04-13 14:46:41 +0530

    first commit from mac

---
+++ firstApplication/firstApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstApplication
+//
+//  Created by Nikhil Bajpai on 12/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("IamStupid")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstApplication/firstApplication/firstApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApplicationApp.swift
+//  firstApplication
+//
+//  Created by Nikhil Bajpai on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct firstApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
